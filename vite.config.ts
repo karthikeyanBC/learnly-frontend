df682aa278d33e29e5669e4e91977dcdf38f8c26
karthikeyanBC <karthikeyan@braincert.com>
2025-11-06 16:22:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['m0wcoss8gkkoswgwc8g44gk4.54.160.166.11.sslip.io']
       },
       plugins: [react()],
       define: {
